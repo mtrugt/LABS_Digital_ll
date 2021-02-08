4eb4dfb6b234d1183603128a97f7672ab567909e
Marco Trujillo <mtrugt@users.noreply.github.com>
2021-02-07 21:17:42 -0600

    Creacion LAB3

---
+++ LAB3/LAB3.X/LAB3lib.h
@@ -0,0 +1,21 @@
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef LAB2lib_H
+#define	LAB2lib_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include <stdint.h>
+
+
+
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
