7af15eb3166c49fcf58ad924c10aed49cfe2d605
Marco Trujillo <mtrugt@users.noreply.github.com>
2021-04-27 21:52:27 -0600

    Game over

--- MINI3/GAME/GAME.ino
+++ MINI3/GAME/GAME.ino
@@ -307,7 +307,8 @@ while(coin != 1){
     k = k + 1;
     delay(30);
     }
-    enemy1 = 0; 
+    enemy1 = 0;
+    punteo = punteo + 1; 
   }
 
   if (enemy2y !=0 && enemy2y < 210){
@@ -322,7 +323,8 @@ while(coin != 1){
     k = k + 1;
     delay(30);
     }
-    enemy2 = 0; 
+    enemy2 = 0;
+    punteo = punteo + 1; 
   }
 
   if (enemy3y !=0 && enemy3y < 210){
@@ -337,7 +339,8 @@ while(coin != 1){
     k = k + 1;
     delay(30);
     }
-    enemy3 = 0; 
+    enemy3 = 0;
+    punteo = punteo + 1; 
   }
 
   if (enemy4y !=0 && enemy4y < 210){
@@ -352,7 +355,8 @@ while(coin != 1){
     k = k + 1;
     delay(30);
     }
-    enemy4 = 0; 
+    enemy4 = 0;
+    punteo = punteo + 1; 
   }
 
 
@@ -361,7 +365,6 @@ while(coin != 1){
   enemyctr = enemyctr + 1;
   if (enemyctr == 31){
     enemyctr = 0;
-    punteo = punteo + 1; 
   }
 
 //Mostrar punteo
@@ -387,58 +390,17 @@ while(coin != 1){
   else if (enemy4y >= 150 && enemy4y <= 197 && enemy4x == posx){
     coin = 1;
   }
-
-  
-  
-}
-  
- /* for(int x = 0; x <320-32; x++){
-    delay(15);
-    int anim2 = (x/35)%2;
-    
-    LCD_Sprite(x,100,16,24,planta,2,anim2,0,1);
-    V_line( x -1, 100, 24, 0x421b);
-    
-    //LCD_Bitmap(x, 100, 32, 32, prueba);
-    
-    int anim = (x/11)%8;
-    
-
-    int anim3 = (x/11)%4;
-    
-    LCD_Sprite(x, 20, 16, 32, mario,8, anim,1, 0);
-    V_line( x -1, 20, 32, 0x421b);
  
-    //LCD_Sprite(x,100,32,32,bowser,4,anim3,0,1);
-    //V_line( x -1, 100, 32, 0x421b);
- 
- 
-    LCD_Sprite(x, 140, 16, 16, enemy,2, anim2,1, 0);
-    V_line( x -1, 140, 16, 0x421b);
-  
-    LCD_Sprite(x, 175, 16, 32, luigi,8, anim,1, 0);
-    V_line( x -1, 175, 32, 0x421b);
   }
-  for(int x = 320-32; x >0; x--){
-    delay(5);
-    int anim = (x/11)%8;
-    int anim2 = (x/11)%2;
-    
-    LCD_Sprite(x,100,16,24,planta,2,anim2,0,0);
-    V_line( x + 16, 100, 24, 0x421b);
-    
-    //LCD_Bitmap(x, 100, 32, 32, prueba);
-    
-    //LCD_Sprite(x, 140, 16, 16, enemy,2, anim2,0, 0);
-    //V_line( x + 16, 140, 16, 0x421b);
-    
-    //LCD_Sprite(x, 175, 16, 32, luigi,8, anim,0, 0);
-    //V_line( x + 16, 175, 32, 0x421b);
 
-    //LCD_Sprite(x, 20, 16, 32, mario,8, anim,0, 0);
-    //V_line( x + 16, 20, 32, 0x421b);
-  } 
-*/
+//Game over y punteo final
+  LCD_Clear(0x605F);
+  LCD_Print("GAME OVER" , 80, 30, 2,   0xFFFF, 0x605F);
+  LCD_Print("Your score: " , 70, 110, 1,   0xFFFF, 0x605F);
+  LCD_Print(puntostxt , 180, 110, 2,   0xFFFF, 0x605F);
+  LCD_Print("Thanks for playing" , 20, 190, 2,   0xFFFF, 0x605F);
+  delay(5000);
+  
 };
 
 
