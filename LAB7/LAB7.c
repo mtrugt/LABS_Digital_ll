128b353737eb00fe4230932716fd140a53f12104
Marco Trujillo <mtrugt@users.noreply.github.com>
2021-05-15 18:54:00 -0600

    Create LAB7.c

---
+++ LAB7/LAB7.c
@@ -0,0 +1,22 @@
+/**
+ * Marco Trujillo
+ * 18069
+ * Digital 2, lab 7
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+
+
+int main(void)
+{
+	return 0;
+}
