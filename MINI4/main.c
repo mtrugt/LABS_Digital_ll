7517ead90bf26046a374055928a1f97c289da91d
Marco Trujillo <mtrugt@users.noreply.github.com>
2021-05-28 12:43:22 -0600

    TIVA LISTO

--- MINI4/main.c
+++ MINI4/main.c
@@ -35,20 +35,23 @@ void DatosUart(char *Dat);
 int main(void)
 {
     //configuracion inicial
-    SysCtlClockSet ( SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL  | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN); //reloj sistema
+    SysCtlClockSet ( SYSCTL_SYSDIV_5 | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN); //reloj sistema
     SysCtlPeripheralEnable ( SYSCTL_PERIPH_GPIOB ); //habilitar reloj puerto B
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)){}
+    SysCtlPeripheralEnable ( SYSCTL_PERIPH_GPIOD ); //habilitar reloj puerto B
+    //while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)){}
     SysCtlPeripheralEnable ( SYSCTL_PERIPH_GPIOA ); //habilitar reloj puerto A
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)){}
+    //while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)){}
     SysCtlPeripheralEnable ( SYSCTL_PERIPH_GPIOF ); //habilitar reloj puerto F
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
+    //while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
 
     GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, BLUE | RED | GREEN); //salidas para led RGB
-    GPIOPinTypeGPIOOutput (GPIO_PORTB_BASE, GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_3 | GPIO_PIN_2); //salidas para semaforos
+
+    GPIOPinTypeGPIOOutput (GPIO_PORTB_BASE, GPIO_PIN_5 | GPIO_PIN_3 | GPIO_PIN_2); //salidas para semaforos
+    GPIOPinTypeGPIOOutput (GPIO_PORTD_BASE, GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_1 | GPIO_PIN_6 | GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_3); //salidas para semaforos
     GPIOPinTypeGPIOOutput (GPIO_PORTA_BASE, GPIO_PIN_5|GPIO_PIN_6);
 
-    GPIOPadConfigSet(GPIO_PORTB_BASE, park1, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPD); //pines, sensores, pulldown
-    GPIOPadConfigSet(GPIO_PORTA_BASE, park2|park3|park4, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPD);
+    GPIOPadConfigSet(GPIO_PORTB_BASE, park1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD); //pines, sensores, pulldown
+    GPIOPadConfigSet(GPIO_PORTA_BASE, park2|park3|park4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
 
     //configurar uart
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);//UART1
@@ -62,30 +65,58 @@ int main(void)
     UARTConfigSetExpClk(UART1_BASE, 16000000, 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |UART_CONFIG_PAR_NONE));
     UARTEnable(UART1_BASE);
 
-    //Obtener Periodo
-    Period = SysCtlClockGet();
+    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0xFF);
+    SysCtlDelay(1000);
+    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0x00);
+
 
 while(1){
     if(GPIOPinRead(GPIO_PORTB_BASE, park1)){ //Si park1 ocupado -> enviar a
-        DatosUart("a"); SysCtlDelay(1000000);}
-    else{
-        DatosUart("b"); SysCtlDelay(1000000);}  //de lo contrario, enviar b}
+        DatosUart("a"); SysCtlDelay(1000000);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0, 0xFF);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0x00);
+
+    }
+    else{//de lo contrario, enviar b
+        DatosUart("b"); SysCtlDelay(1000000);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0, 0x00);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0xFF);
+
+    }
 
     if(GPIOPinRead(GPIO_PORTA_BASE, park2)){
-        DatosUart("c"); SysCtlDelay(1000000);}
+        DatosUart("c"); SysCtlDelay(1000000);
+        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_5, 0xFF);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_6, 0x00);
+    }
     else{
-        DatosUart("d"); SysCtlDelay(1000000);}
+        DatosUart("d"); SysCtlDelay(1000000);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_6, 0xFF);
+        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_5, 0x00);
+    }
 
     if(GPIOPinRead(GPIO_PORTA_BASE, park3)){
-        DatosUart("e"); SysCtlDelay(1000000);}
+        DatosUart("e"); SysCtlDelay(1000000);
+        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_3, 0xFF);
+        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, 0x00);
+    }
     else{
-        DatosUart("f"); SysCtlDelay(1000000);}
-
+        DatosUart("f"); SysCtlDelay(1000000);
+        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, 0xFF);
+        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_3, 0x00);
+    }
 
     if(GPIOPinRead(GPIO_PORTA_BASE, park4)){
-        DatosUart("g"); SysCtlDelay(1000000);}
+        DatosUart("g"); SysCtlDelay(1000000);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0xFF);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 0x00);
+    }
     else{
-        DatosUart("h"); SysCtlDelay(1000000);}
+        DatosUart("h"); SysCtlDelay(1000000);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 0xFF);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0x00);
+
+    }
 
 
     SysCtlDelay(10000);
